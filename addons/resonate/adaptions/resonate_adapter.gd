b46b0ec1d9fe74d5a2f6a4efd119f19d04417962
winkensjw <winkensjw@gmail.com>
2025-03-15 17:41:04 +0100

    General code cleanup

--- addons/resonate/adaptions/resonate_adapter.gd
+++ addons/resonate/adaptions/resonate_adapter.gd
@@ -11,6 +11,10 @@
 
 extends Node
 
+## Logger instance for this class.
+## It is initialized with the name of this script's class.
+var _log: Log = Log.new(self.name)
+
 
 ## Called when the node is initialized.
 func _ready() -> void:
@@ -19,20 +23,20 @@ func _ready() -> void:
 
 ## Called when the game's settings have changed.
 func _on_settings_changed() -> void:
-	Log.debug("Settings changed, updating volume")
+	_log.debug("Settings changed, updating volume")
 	update_volume()
 
 
 ## Calculates the music volume in decibels.
 ## @return The music volume in decibels.
 func calc_music_volume_db() -> float:
-	return minf(Globals.master_volume_db, Globals.music_volume_db)
+	return minf(Globals.get_master_volume_db(), Globals.get_music_volume_db())
 
 
 ## Calculates the game volume in decibels.
 ## @return The game volume in decibels.
 func calc_game_volume_db() -> float:
-	return minf(Globals.master_volume_db, Globals.game_volume_db)
+	return minf(Globals.get_master_volume_db(), Globals.get_game_volume_db())
 
 
 ## Updates the volume based on the game's settings.
@@ -45,7 +49,7 @@ func update_volume() -> void:
 ## @param bank_name The name of the music bank to play from.
 ## @param track_name The name of the music track to play.
 func play_music(bank_name: String, track_name: String) -> void:
-	Log.debug("Playing music track '%s' from bank '%s'" % [track_name, bank_name])
+	_log.debug("Playing music track '%s' from bank '%s'" % [track_name, bank_name])
 	MusicManager.play(bank_name, track_name)
 
 
@@ -61,5 +65,5 @@ func play_sound(bank_name: String, track_name: String) -> void:
 ## @param track_name The name of the sound effect to play.
 ## @param volume The volume to play the sound effect at.
 func play_sound_at_volume(bank_name: String, track_name: String, volume: float) -> void:
-	Log.debug("Playing sound effect '%s' from bank '%s' at volume %f dB" % [track_name, bank_name, volume])
+	_log.debug("Playing sound effect '%s' from bank '%s' at volume %f dB" % [track_name, bank_name, volume])
 	SoundManager.play_varied(bank_name, track_name, 1.0, volume, "")
